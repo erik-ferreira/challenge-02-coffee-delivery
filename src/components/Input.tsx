79588dbabeb06da5dfa531183b3677edea5229e6
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-08 11:29:37 -0300

    create variants input; complete screen checkout

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,29 @@
 import { InputHTMLAttributes } from "react";
 
+const widthsInput = {
+  normal: "w-52",
+  min: "w-16",
+  max: "w-full",
+  flex: "flex-1",
+};
+
+type WidthsInputType = keyof typeof widthsInput;
+
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isOptional?: boolean;
+  variantWidth?: WidthsInputType;
 }
 
-export function Input({ isOptional, ...rest }: InputProps) {
+export function Input({
+  isOptional,
+  variantWidth = "normal",
+
+  ...rest
+}: InputProps) {
   return (
-    <div className="w-52 h-11 bg-gray-150 flex items-center rounded border border-solid border-gray-200">
+    <div
+      className={`${widthsInput[variantWidth]} h-11 bg-gray-150 flex items-center rounded border border-solid border-gray-200`}
+    >
       <input
         className="w-full h-full rounded bg-transparent placeholder-brow-400 text-brow-500 text-sm font-sans leading-[1.3] outline-none px-3 focus:border-yellow-600"
         {...rest}
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -16,154 +16,171 @@ import americanPNG from "../assets/coffees/americano.png";
 
 export function Checkout() {
   return (
-    <>
-      <h2 className="font-cursive font-bold text-lg text-brow-600">
-        Complete seu pedido
-      </h2>
+    <div className="flex gap-8 max-w-[1120px] w-[90%] mx-auto">
+      <div>
+        <h2 className="font-cursive font-bold text-lg text-brow-600 mb-4">
+          Complete seu pedido
+        </h2>
 
-      <div className="bg-gray-100 rounded-md p-10 w-[640px]">
-        <div className="flex gap-2">
-          <MapPinLine size={24} className="text-yellow-600 " />
-          <span className="font-sans text-base text-brow-600">
-            Endereço de entrega
-          </span>
-        </div>
-
-        <p className="font-sans text-sm text-brow-500 ml-8">
-          Informe o endereço onde deseja receber seu pedido
-        </p>
-
-        <Input placeholder="CEP" />
-        <Input placeholder="Rua" />
-        <Input placeholder="Número" />
-        <Input placeholder="Complemento" isOptional />
-        <Input placeholder="Bairro" />
-        <Input placeholder="Cidade" />
-        <Input placeholder="UF" />
-      </div>
-
-      <div className="bg-gray-100 rounded-md p-10 w-[640px] mt-3">
-        <div className="flex gap-2">
-          <CurrencyDollar size={24} className="text-violet-600 " />
-          <span className="font-sans text-base text-brow-600">Pagamento</span>
-        </div>
-
-        <p className="font-sans text-sm text-brow-500 ml-8">
-          O pagamento é feito na entrega. Escolha a forma que deseja pagar
-        </p>
-
-        <div className="flex gap-3 mt-8">
-          <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
-            <CreditCard size={16} className="text-violet-600 " />
-            CARTÃO DE CRÉDITO
-          </button>
-
-          <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
-            <Money size={16} className="text-violet-600 " />
-            CARTÃO DE DÉBITO
-          </button>
-
-          <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
-            <Bank size={16} className="text-violet-600 " />
-            DINHEIRO
-          </button>
-        </div>
-      </div>
-
-      <div className="bg-gray-100 p-10 w-[448px] mt-3 rounded-tl-md rounded-tr-[44px] rounded-br-md rounded-bl-[44px] flex justify-center flex-col gap-6">
-        <div className="flex gap-5">
-          <img src={americanPNG} className="w-16 h-16" />
-
-          <div className="flex gap-2 flex-col">
+        <div className="bg-gray-100 rounded-md p-10">
+          <div className="flex gap-2">
+            <MapPinLine size={24} className="text-yellow-600 " />
             <span className="font-sans text-base text-brow-600">
-              Expresso Tradicional
+              Endereço de entrega
             </span>
-
-            <div className="flex gap-2">
-              <div className="w-20 h-10 rounded-md bg-gray-200 flex items-center justify-center gap-2 ">
-                <button>
-                  <Minus size={14} className="text-violet-600" />
-                </button>
-
-                <span className="font-sans text-base leading-[1.3] text-brow-900">
-                  1
-                </span>
-
-                <button>
-                  <Plus size={14} className="text-violet-600" />
-                </button>
-              </div>
-
-              <button className="bg-gray-200 rounded-md p-2 flex items-center gap-1 font-sans text-xs uppercase text-brow-500">
-                <Trash size={14} className="text-violet-600" />
-                Remover
-              </button>
-            </div>
           </div>
 
-          <span className="font-sans text-base font-bold text-brow-500">
-            R$ 9,90
-          </span>
-        </div>
+          <p className="font-sans text-sm text-brow-500 ml-8">
+            Informe o endereço onde deseja receber seu pedido
+          </p>
 
-        <div className="h-[1px] w-full bg-gray-200" />
+          <div className="mt-8 flex flex-col gap-4">
+            <Input placeholder="CEP" />
 
-        <div className="flex gap-5">
-          <img src={americanPNG} className="w-16 h-16" />
+            <Input placeholder="Rua" variantWidth="max" />
 
-          <div className="flex gap-2 flex-col">
-            <span className="font-sans text-base text-brow-600">
-              Expresso Tradicional
-            </span>
+            <div className="flex gap-3">
+              <Input placeholder="Número" />
+              <Input placeholder="Complemento" isOptional variantWidth="flex" />
+            </div>
 
-            <div className="flex gap-2">
-              <div className="w-20 h-10 rounded-md bg-gray-200 flex items-center justify-center gap-2 ">
-                <button>
-                  <Minus size={14} className="text-violet-600" />
-                </button>
-
-                <span className="font-sans text-base leading-[1.3] text-brow-900">
-                  1
-                </span>
-
-                <button>
-                  <Plus size={14} className="text-violet-600" />
-                </button>
-              </div>
-
-              <button className="bg-gray-200 rounded-md p-2 flex items-center gap-1 font-sans text-xs uppercase text-brow-500">
-                <Trash size={14} className="text-violet-600" />
-                Remover
-              </button>
+            <div className="flex gap-3">
+              <Input placeholder="Bairro" />
+              <Input placeholder="Cidade" />
+              <Input placeholder="UF" variantWidth="min" />
             </div>
           </div>
-
-          <span className="font-sans text-base font-bold text-brow-500">
-            R$ 9,90
-          </span>
         </div>
 
-        <div className="h-[1px] w-full bg-gray-200" />
+        <div className="bg-gray-100 rounded-md p-10 mt-3">
+          <div className="flex gap-2">
+            <CurrencyDollar size={24} className="text-violet-600 " />
+            <span className="font-sans text-base text-brow-600">Pagamento</span>
+          </div>
 
-        <div className="flex flex-col gap-3">
-          <span className="font-sans text-sm text-brow-500 flex items-center justify-between">
-            Total de itens
-            <span>R$ 29,70</span>
-          </span>
+          <p className="font-sans text-sm text-brow-500 ml-8">
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
 
-          <span className="font-sans text-sm text-brow-500 flex items-center justify-between">
-            Entrega
-            <span>R$ 3,50</span>
-          </span>
+          <div className="flex gap-3 mt-8">
+            <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
+              <CreditCard size={16} className="text-violet-600 " />
+              CARTÃO DE CRÉDITO
+            </button>
 
-          <span className="font-sans font-bold text-xl text-brow-500 flex items-center justify-between">
-            Total
-            <span>R$ 33,20</span>
-          </span>
+            <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
+              <Money size={16} className="text-violet-600 " />
+              CARTÃO DE DÉBITO
+            </button>
+
+            <button className="bg-gray-200 rounded-md p-4 flex gap-3 items-center text-brow-500 font-sans text-xs uppercase">
+              <Bank size={16} className="text-violet-600 " />
+              DINHEIRO
+            </button>
+          </div>
         </div>
-
-        <Button label="Confirmar Pedido" />
       </div>
-    </>
+
+      <div>
+        <h2 className="font-cursive font-bold text-lg text-brow-600 mb-4">
+          Cafés selecionados
+        </h2>
+
+        <div className="bg-gray-100 p-10 w-[448px] rounded-tl-md rounded-tr-[44px] rounded-br-md rounded-bl-[44px] flex justify-center flex-col gap-6">
+          <div className="flex gap-5">
+            <img src={americanPNG} className="w-16 h-16" />
+
+            <div className="flex gap-2 flex-col">
+              <span className="font-sans text-base text-brow-600">
+                Expresso Tradicional
+              </span>
+
+              <div className="flex gap-2">
+                <div className="w-20 h-10 rounded-md bg-gray-200 flex items-center justify-center gap-2 ">
+                  <button>
+                    <Minus size={14} className="text-violet-600" />
+                  </button>
+
+                  <span className="font-sans text-base leading-[1.3] text-brow-900">
+                    1
+                  </span>
+
+                  <button>
+                    <Plus size={14} className="text-violet-600" />
+                  </button>
+                </div>
+
+                <button className="bg-gray-200 rounded-md p-2 flex items-center gap-1 font-sans text-xs uppercase text-brow-500">
+                  <Trash size={14} className="text-violet-600" />
+                  Remover
+                </button>
+              </div>
+            </div>
+
+            <span className="font-sans text-base font-bold text-brow-500">
+              R$ 9,90
+            </span>
+          </div>
+
+          <div className="h-[1px] w-full bg-gray-200" />
+
+          <div className="flex gap-5">
+            <img src={americanPNG} className="w-16 h-16" />
+
+            <div className="flex gap-2 flex-col">
+              <span className="font-sans text-base text-brow-600">
+                Expresso Tradicional
+              </span>
+
+              <div className="flex gap-2">
+                <div className="w-20 h-10 rounded-md bg-gray-200 flex items-center justify-center gap-2 ">
+                  <button>
+                    <Minus size={14} className="text-violet-600" />
+                  </button>
+
+                  <span className="font-sans text-base leading-[1.3] text-brow-900">
+                    1
+                  </span>
+
+                  <button>
+                    <Plus size={14} className="text-violet-600" />
+                  </button>
+                </div>
+
+                <button className="bg-gray-200 rounded-md p-2 flex items-center gap-1 font-sans text-xs uppercase text-brow-500">
+                  <Trash size={14} className="text-violet-600" />
+                  Remover
+                </button>
+              </div>
+            </div>
+
+            <span className="font-sans text-base font-bold text-brow-500">
+              R$ 9,90
+            </span>
+          </div>
+
+          <div className="h-[1px] w-full bg-gray-200" />
+
+          <div className="flex flex-col gap-3">
+            <span className="font-sans text-sm text-brow-500 flex items-center justify-between">
+              Total de itens
+              <span>R$ 29,70</span>
+            </span>
+
+            <span className="font-sans text-sm text-brow-500 flex items-center justify-between">
+              Entrega
+              <span>R$ 3,50</span>
+            </span>
+
+            <span className="font-sans font-bold text-xl text-brow-500 flex items-center justify-between">
+              Total
+              <span>R$ 33,20</span>
+            </span>
+          </div>
+
+          <Button label="Confirmar Pedido" />
+        </div>
+      </div>
+    </div>
   );
 }
